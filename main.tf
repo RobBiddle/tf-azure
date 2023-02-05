16834167a1bf9421a9ec167bcd50be406d7cd4f6
Robert Biddle <RobBiddle@users.noreply.github.com>
2023-02-05 14:22:15 -0500

    adding resources to test GitHub Actions Workflow

--- main.tf
+++ main.tf
@@ -2,10 +2,19 @@ resource "azurerm_resource_group" "rg" {
   name     = "myTFResourceGroup"
   location = "eastus"
 }
-
+resource "azurerm_resource_group" "rg2" {
+  name     = "myTFResourceGroup2"
+  location = "eastus"
+}
 resource "azurerm_virtual_network" "vnet" {
   name                = "myTFVnet"
-  address_space       = ["10.0.0.0/16"]
+  address_space       = ["10.1.0.0/16"]
+  location            = "eastus"
+  resource_group_name = azurerm_resource_group.rg.name
+}
+resource "azurerm_virtual_network" "vnet2" {
+  name                = "myTFVnet2"
+  address_space       = ["10.2.0.0/16"]
   location            = "eastus"
   resource_group_name = azurerm_resource_group.rg.name
 }
